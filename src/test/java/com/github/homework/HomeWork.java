d6b9b4e980061ac6d76c3e646dd5ebab03fee9eb
AMosk <poostoffice@gmail.com>
2022-10-22 22:13:29 +0300

    QA2 HomeWork - Objects and code Run.

---
+++ src/test/java/com/github/homework/HomeWork.java
@@ -0,0 +1,180 @@
+package com.github.homework;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeWork {
+
+    private String color;
+    private String model;
+    private String manufacturer;
+    private String fuel;
+    private String name;
+    private String address;
+    private String producingCountry;
+    private double weight;
+    private double square;
+    private double width;
+    private double priceForMeter;
+    private double height;
+    private int roomCount;
+    private int yearOfIssue;
+    private int floor;
+    private int itemCount;
+    private int age;
+    private float totalPrice;
+    private boolean discountRequested;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProducingCountry() {
+        return producingCountry;
+    }
+
+    public void setProducingCountry(String producingCountry) {
+        this.producingCountry = producingCountry;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getPriceForMeter() {
+        return priceForMeter;
+    }
+
+    public void setPriceForMeter(double priceForMeter) {
+        this.priceForMeter = priceForMeter;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getRoomCount() {
+        return roomCount;
+    }
+
+    public void setRoomCount(int roomCount) {
+        this.roomCount = roomCount;
+    }
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public void setYearOfIssue(int yearOfIssue) {
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean isDiscountRequested() {
+        return discountRequested;
+    }
+
+    public void setDiscountRequested(boolean discountRequested) {
+        this.discountRequested = discountRequested;
+    }
+}
+
+ 
---
+++ src/test/java/com/github/homework/ObjectsMaking.java
@@ -0,0 +1,95 @@
+package com.github.homework;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ObjectsMaking {
+@Test
+        public void workingWithHomeWork(){
+
+    HomeWork car = new HomeWork();
+    car.setColor ("green");
+    car.setModel ("Audi");
+    car.setFuel("Diesel");
+    car.setManufacturer("Germany");
+    car.setYearOfIssue(1);
+    car.setYearOfIssue(2022);
+    car.setTotalPrice(50000);
+
+    HomeWork house = new HomeWork();
+    house.setAddress("Eksporta 1-1, Riga, LV-1013");
+    house.setFloor(5);
+    house.setSquare(333.33);
+    house.setName("RiverSideHouse");
+    house.setItemCount(1);
+    house.setTotalPrice(250000);
+    house.setRoomCount(5);
+    house.isDiscountRequested();
+    house.setPriceForMeter(2500.50);
+
+    HomeWork box = new HomeWork();
+    box.setColor("grey");
+    box.setManufacturer("China");
+    box.setHeight(250);
+    box.setWidth(200);
+    box.setItemCount(1);
+    box.setTotalPrice(50000);
+    box.setItemCount(1);
+
+    HomeWork phone = new HomeWork();
+    phone.setColor("grey");
+    phone.setManufacturer("Samsung");
+    phone.setModel("S22");
+    phone.setItemCount(5);
+    phone.setTotalPrice(3500);
+
+    HomeWork table = new HomeWork();
+    table.setModel("TableA");
+    table.setColor("white");
+    table.setHeight(60);
+    table.setWeight(20.500);
+    table.setWidth(55.50);
+
+    HomeWork window = new HomeWork();
+    window.setWidth(90);
+    window.setHeight(120);
+    window.setWeight(15);
+    window.setItemCount(2);
+    window.setTotalPrice(500);
+
+    HomeWork bag = new HomeWork();
+    bag.setColor("blue");
+    bag.setWeight(1.250);
+    bag.setHeight(20.50);
+    bag.setWidth(15.50);
+    bag.setItemCount(1);
+    bag.setTotalPrice(100);
+
+    HomeWork book = new HomeWork();
+    book.setColor("black");
+    book.setItemCount(4);
+    book.setWeight(2.535);
+    book.setTotalPrice(80);
+    book.setName("Java - basic course");
+
+    HomeWork gift = new HomeWork();
+    gift.setColor("red");
+    gift.setItemCount(10);
+    gift.setTotalPrice(333);
+    gift.setWeight(4.44444);
+    gift.setYearOfIssue(2010);
+
+    HomeWork ski = new HomeWork();
+    ski.setColor("Red and White");
+    ski.setModel("Atomic");
+    ski.setWeight(3.50);
+    ski.setItemCount(1);
+    ski.setTotalPrice(777);
+
+    System.out.println("Vasya kupil" + "Car" + "i kvartiru tozhe" + "House" + "Phone" + "Book" + "i esche on kupil");
+    System.out.println(car.getColor() + car.getModel() + car.getTotalPrice());
+    System.out.println(house.getFloor() + house.getAddress() + house.getRoomCount() + house.getTotalPrice());
+    System.out.println(phone.getColor() + phone.getModel() + phone.getTotalPrice());
+    System.out.println(ski.getColor() + ski.getModel() + ski.getTotalPrice());
+    }
+}
